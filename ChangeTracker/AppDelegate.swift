f19e1f05b516aa592476b77317002c2a2c424eac
Tim Woodford <timthewoodford@gmail.com>
2018-06-04 13:45:17 -0400

    Initial Commit

---
+++ ChangeTracker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ChangeTracker
+//
+//  Created by Tim on 6/4/18.
+//  Copyright © 2018 Tim Woodford. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ChangeTracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChangeTracker
+//
+//  Created by Tim on 6/4/18.
+//  Copyright © 2018 Tim Woodford. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
