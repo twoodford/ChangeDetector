f86893f2b55b8f39418ade2338c1d6a55cb56fec
Tim Woodford <timthewoodford@gmail.com>
2018-07-02 15:55:34 -0400

    Add new executable, move some files around

---
+++ changetrackupdater/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  changetrackupdater
+//
+//  Created by Tim on 7/2/18.
+//  Copyright © 2018 Tim Woodford. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
