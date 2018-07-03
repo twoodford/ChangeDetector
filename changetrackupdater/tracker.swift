f8ef3dd3a668be1dc18c4776fd7072468ae58d32
Tim Woodford <timthewoodford@gmail.com>
2018-07-02 21:54:20 -0400

    Fix how notifications are implemented for background processes
    At this point, this should be considered a stopgap until I find a better way to do this

--- changetrackupdater/tracker.swift
+++ changetrackupdater/tracker.swift
@@ -9,6 +9,24 @@
 import Foundation
 import ChangeTracking
 
+func __osaSendNotification(withText text: String) {
+//    print("testing")
+//    if let osaObj = NSAppleScript(source: "display notification \""+text+"\" with title \"ChangeDetector\"") {
+//        var error: NSDictionary?
+//        osaObj.executeAndReturnError(&error)
+//        if error != nil {
+//            print("OSAScript error: ", error)
+//        }
+//    } else {
+//        print("syntax error")
+//    }
+    let task = Process()
+    task.launchPath="/usr/bin/osascript"
+    task.arguments = ["-e", "display notification \""+text+"\" with title \"ChangeDetector\""]
+    task.launch()
+    task.waitUntilExit()
+}
+
 class Tracker {
     var changes : [UUID: [ChangeDescription]] = [:]
     var delay = 30 // TODO better delay control (1 hour)
@@ -55,9 +73,10 @@ class Tracker {
     }
     
     func sendChangeNotification() {
-        let nreq = NSUserNotification()
-        nreq.informativeText = "New file changes detected"
-        NSUserNotificationCenter.default.deliver(nreq)
+//        let nreq = NSUserNotification()
+//        nreq.informativeText = "New file changes detected"
+//        NSUserNotificationCenter.default.deliver(nreq)
+        __osaSendNotification(withText: "New file changes detected")
     }
     
     func _storeChanges(uuid: UUID, addChanges: [ChangeDescription]) {
@@ -73,29 +92,34 @@ class Tracker {
         var newChanges = false
         let paths = defaults.getPaths()
         for path in paths {
-            let trackDataURL = filePath(forUUID: path.id)
-            if changes[path.id] == nil { changes[path.id] = [] }
-            // Decode change checking data
-            if let decodedDat = try? JSONSerialization.jsonObject(with: Data(contentsOf: trackDataURL), options: []) as! [String:Any]{
-                if let trackerType = decodedDat["tracker"] as? String {
-                    let tracker = getTracker(trackerID: trackerType)!
-                    tracker.setTrackData(baseURL: path.url, dat: decodedDat["data"]!)
-                    let addChanges = tracker.didChange()
-                    _storeChanges(uuid: path.id, addChanges: addChanges)
-                    if addChanges.count > 0 {
-                        newChanges = true
-                    }
-                    try! trackerData(tracker: tracker, trackerType: trackerType).write(to: trackDataURL)
-                } else {
-                    print("error: could not find tracker class")
-                }
-            } else {
-                addPath(path: path)
-            }
+            newChanges = newChanges || changeCheck(path: path)
         }
         if newChanges {
             sendChangeNotification()
         }
-        scheduleUpdater(delaySeconds: delay)
+    }
+    
+    func changeCheck(path: TrackedURL) -> Bool {
+        var newChanges = false
+        let trackDataURL = filePath(forUUID: path.id)
+        if changes[path.id] == nil { changes[path.id] = [] }
+        // Decode change checking data
+        if let decodedDat = try? JSONSerialization.jsonObject(with: Data(contentsOf: trackDataURL), options: []) as! [String:Any]{
+            if let trackerType = decodedDat["tracker"] as? String {
+                let tracker = getTracker(trackerID: trackerType)!
+                tracker.setTrackData(baseURL: path.url, dat: decodedDat["data"]!)
+                let addChanges = tracker.didChange()
+                _storeChanges(uuid: path.id, addChanges: addChanges)
+                if addChanges.count > 0 {
+                    newChanges = true
+                }
+                try! trackerData(tracker: tracker, trackerType: trackerType).write(to: trackDataURL)
+            } else {
+                print("error: could not find tracker class")
+            }
+        } else {
+            addPath(path: path)
+        }
+        return newChanges
     }
 }
