f7e04f30425fc4120a51910895228da3604bcecb
Tim Woodford <timthewoodford@gmail.com>
2018-07-14 13:36:47 -0400

    Try to improve placement of ellipses
    This doesn't work yet, so I'm leaving it in a branch to revisit in the
    future

--- ChangeDetector/formatters.swift
+++ ChangeDetector/formatters.swift
@@ -20,3 +20,38 @@ class CTURLFormatter : Formatter {
         return "(URL unavailable)"
     }
 }
+
+// Roughly based on http://www.cocoabuilder.com/archive/cocoa/65467-nstableview-and-ellipses-again.html
+class CTURLSizeFormatter : CTURLFormatter {
+    @IBOutlet var tableCol: NSTableColumn!
+    @IBOutlet var table: NSTableView!
+    
+    override func string(for obj: Any?) -> String {
+        if let url = obj as? URL {
+            if url.isFileURL {
+                print("hi!")
+                let cell = tableCol.dataCell as! NSCell
+                let tgWidth = cell.titleRect(forBounds: NSRect(x: 0, y: 0, width: tableCol.width, height: table.rowHeight)).width
+                let txtattr = [NSAttributedStringKey.font: cell.font!]
+                var components = url.pathComponents.count
+                while strify(url: url, numComponents: components).size(withAttributes: txtattr).width > tgWidth {
+                    print(strify(url: url, numComponents: components))
+                    components = components - 1
+                }
+                return strify(url: url, numComponents: components) as String
+            }
+        }
+        return super.string(for: obj)
+    }
+    
+    func strify(url: URL, numComponents num: Int) -> NSString {
+        // Current hueristic: include 1st and last component if possible
+        // If we can only have 1, include only the last
+        // Otherwise, prioritise later components over ealier ones
+        var x="x"
+        for i in 1...num {
+            x = x+"x"
+        }
+        return x as NSString
+    }
+}
