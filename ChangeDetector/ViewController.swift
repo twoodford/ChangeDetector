3e53385ce51b16cc4c251265a80626e836d4417b
Tim Woodford <timthewoodford@gmail.com>
2018-08-23 00:09:39 -0400

    Remove base path from detected change paths

--- ChangeDetector/ViewController.swift
+++ ChangeDetector/ViewController.swift
@@ -51,13 +51,20 @@ class ViewController: NSViewController {
         let selPath = (URLArrayController.arrangedObjects as! [TrackedURL])[URLRow]
         
         let changes = changeStore.getChanges(forUUID: selPath.id)
+        let basePathLen = selPath.url.path.count
+        
+        let truncatedCh = changes.map({(dc: DetectedChange) -> (DetectedChange) in
+            let str = dc.path!
+            dc.path = String(str[str.index(str.startIndex, offsetBy: basePathLen+1)...])
+            return dc
+        })
         
         // Clear previous
         let len = (self.ChangesArrayController.arrangedObjects as! [ChangeDescription]).count
         self.ChangesArrayController.remove(atArrangedObjectIndexes: IndexSet(integersIn: 0..<len))
         
         // Add new paths
-        self.ChangesArrayController.add(contentsOf: changes)
+        self.ChangesArrayController.add(contentsOf: truncatedCh)
     }
     
     func statusStartManualUpdate() {
